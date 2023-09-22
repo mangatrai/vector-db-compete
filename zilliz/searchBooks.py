9287e2d8b05655bd77b038d055cbcabec7317b8d
Mangat Rai <mangatrai@gmail.com>
2023-09-22 00:29:01 -0500

    Update searchBooks.py
    removed password

--- zilliz/searchBooks.py
+++ zilliz/searchBooks.py
@@ -14,16 +14,16 @@ def embed(text):
         engine=OPENAI_ENGINE)["data"][0]["embedding"]
 
 # Define parameters
-FILE = '/Users/mangat.rai/datastax/zilliz/books.csv'  # Download it from https://www.kaggle.com/datasets/jealousleopard/goodreadsbooks and save it in the folder that holds your script.
+FILE = 'books.csv'  # Download it from https://www.kaggle.com/datasets/jealousleopard/goodreadsbooks and save it in the folder that holds your script.
 COLLECTION_NAME = 'title_db'  # Collection name
 DIMENSION = 1536  # Embeddings size
 COUNT = 200  # How many titles to embed and insert
-URI = 'https://in03-a33c2f70e909d9a.api.gcp-us-west1.zillizcloud.com'  # Endpoint URI obtained from Zilliz Cloud
-USER = 'mangatrai@duck.com'  # Username specified when you created this cluster
-PASSWORD='jwj3dzr2qge6cjq*YRZ'  # Password set for that account
+URI = ''  # Endpoint URI obtained from Zilliz Cloud
+USER = ''  # Username specified when you created this cluster
+PASSWORD=''  # Password set for that account
 OPENAI_ENGINE = 'text-embedding-ada-002'  # Which engine to use
-openai.api_key = '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'  # Use your own Open AI API Key here
-zilliz_api_key = 'fd3cf5462d31b7e4ede7344beaeec7c1a76ee4027064b8ee8a3084916cb9d5e35edd99e654a8f254fc3bae13b9d1208b1dec4d85' # Use your own Zilliz Cloud API key here
+openai.api_key = ''  # Use your own Open AI API Key here
+zilliz_api_key = '' # Use your own Zilliz Cloud API key here
 
 # For serverless cluster
 TOKEN = zilliz_api_key
